098c4e232d7d1bfbe2122ac7bf9b747ddda822bd
Coppola Gianfranco <gianf.coppola@gmail.com>
2022-11-30 23:24:06 +0100

    security added

--- src/main/java/com/gianfcop/ssd/controller/CentroSportivoController.java
+++ src/main/java/com/gianfcop/ssd/controller/CentroSportivoController.java
@@ -2,12 +2,16 @@ package com.gianfcop.ssd.controller;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +29,10 @@ import com.gianfcop.ssd.dto.NumeroPrenotazioniDTO;
 import com.gianfcop.ssd.model.CentroSportivo;
 import com.gianfcop.ssd.service.CentroSportivoService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class CentroSportivoController {
 
 
@@ -38,7 +45,14 @@ public class CentroSportivoController {
     }
 
     @GetMapping("/centro-sportivo/dati")
-    public String datiCentroSportivo(Model model){
+    public String datiCentroSportivo(Model model, @AuthenticationPrincipal Jwt jwt){
+       
+        String roles = jwt.getClaimAsMap("realm_access").get("roles").toString();
+        List<String> roleList = Arrays.stream(roles.replace("[","").replace("]", "").replace("\"", "").split(",")).toList();
+        if(!roleList.contains("admin")){
+            log.error("ACCESS FORBIDDEN 403");
+            return "access_forbidden";
+        }
         model.addAttribute("datiPrenotazioni", centroSportivoService.getDatiPrenotazioni());
         model.addAttribute("datiAbbonamenti", centroSportivoService.getDatiAbbonamenti());
         model.addAttribute("datiIncassi", centroSportivoService.getIncassiCentroSportivo());
@@ -46,7 +60,14 @@ public class CentroSportivoController {
     }
 
     @GetMapping("/centro-sportivo/prenotazioni/data-analysis")
-    public String analisiDatiPrenotazioni(Model model){
+    public String analisiDatiPrenotazioni(Model model, @AuthenticationPrincipal Jwt jwt){
+       
+        String roles = jwt.getClaimAsMap("realm_access").get("roles").toString();
+        List<String> roleList = Arrays.stream(roles.replace("[","").replace("]", "").replace("\"", "").split(",")).toList();
+        if(!roleList.contains("admin_centro_sportivo")){
+            log.error("ACCESS FORBIDDEN 403");
+            return "access_forbidden";
+        }
         NumeroPrenotazioniDTO numeroPrenotazioniDTO = centroSportivoService.getDatiPrenotazioni();
         Map<String, Integer> graphData = new TreeMap<>();
         graphData.put("calcio", numeroPrenotazioniDTO.getNumeroPrenotazioniCalcio());
@@ -65,7 +86,15 @@ public class CentroSportivoController {
     }
 
     @GetMapping("/centro-sportivo/abbonamenti/data-analysis")
-    public String analisiDatiAbbonamenti(Model model){
+    public String analisiDatiAbbonamenti(Model model, @AuthenticationPrincipal Jwt jwt){
+        
+        String roles = jwt.getClaimAsMap("realm_access").get("roles").toString();
+        List<String> roleList = Arrays.stream(roles.replace("[","").replace("]", "").replace("\"", "").split(",")).toList();
+        if(!roleList.contains("admin")){
+            log.error("ACCESS FORBIDDEN 403");
+            return "access_forbidden";
+        }
+
         NumeroAbbonamentiDTO numeroAbbonamentiDTO = centroSportivoService.getDatiAbbonamenti();
         Map<String, Integer> graphData = new TreeMap<>();
         
@@ -83,7 +112,15 @@ public class CentroSportivoController {
     }
 
     @GetMapping("/centro-sportivo/incassi/data-analysis")
-    public String analisiIncassiCentroSportivo(Model model){
+    public String analisiIncassiCentroSportivo(Model model, @AuthenticationPrincipal Jwt jwt){
+        
+        String roles = jwt.getClaimAsMap("realm_access").get("roles").toString();
+        List<String> roleList = Arrays.stream(roles.replace("[","").replace("]", "").replace("\"", "").split(",")).toList();
+        if(!roleList.contains("admin")){
+            log.error("ACCESS FORBIDDEN 403");
+            return "access_forbidden";
+        }
+
         IncassiDTO incassiDTO = centroSportivoService.getIncassiCentroSportivo();
         Map<String, Integer> graphData = new TreeMap<>();
         
--- src/main/java/com/gianfcop/ssd/security/SecurityConfig.java
+++ src/main/java/com/gianfcop/ssd/security/SecurityConfig.java
@@ -1,17 +1,11 @@
 package com.gianfcop.ssd.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-//import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
 
 @Configuration
 @EnableWebSecurity
@@ -23,15 +17,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
-		http
-        // remove csrf and state in session because in jwt we do not need them
-        .csrf().disable()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authorizeRequests()
-        //.antMatchers("/api/public/management/*").hasRole("MANAGER")
-        //.antMatchers("/api/public/admin/*").hasRole("ADMIN")
-        .anyRequest().permitAll();
+        http
+        .authorizeRequests(authorize -> authorize
+				.antMatchers("/strutture/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 	}
 	
 	
